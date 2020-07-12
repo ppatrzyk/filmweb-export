a6ccc8bc09fa1dd1742a03346395e231bc2987b6
ppatrzyk <piotr@patrzyk.me>
2020-07-12 17:00:45 +0200

    get id function

--- filmweb-export/utils.py
+++ filmweb-export/utils.py
@@ -1,19 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-user_name = 'pieca'
-
-fw_url = 'https://www.filmweb.pl/user/{}'.format(user_name)
-try:
-    fw_page = requests.get(fw_url)
-except Exception as e:
-    pass
-soup = BeautifulSoup(fw_page.content, "html.parser")
-fw_preview = soup.find('div', class_='userPreview')
-try:
+def get_user_id(user_name):
+    """
+    Check user ID based on user name
+    """
+    fw_url = f'https://www.filmweb.pl/user/{user_name}'
+    try:
+        fw_page = requests.get(fw_url)
+    except Exception as e:
+        raise ValueError(f'Connection to filmweb.pl failed: {str(e)}')
+    soup = BeautifulSoup(fw_page.content, "html.parser")
+    fw_preview = soup.find('div', class_='userPreview')
     fw_uid = fw_preview['data-id']
-    assert fw_uid != '$user.id'
-except Exception as e:
-    pass
+    assert fw_uid != '$user.id', f'user {user_name} does not exist'
+    return fw_uid
 
-print(fw_uid)
+print(get_user_id('pieca'))
