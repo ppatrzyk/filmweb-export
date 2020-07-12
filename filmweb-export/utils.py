4a39bb656a9819c69548fb57c824f8e42de9b9c7
ppatrzyk <piotr@patrzyk.me>
2020-07-12 17:03:30 +0200

    try-except id extraction

--- filmweb-export/utils.py
+++ filmweb-export/utils.py
@@ -10,10 +10,11 @@ def get_user_id(user_name):
         fw_page = requests.get(fw_url)
     except Exception as e:
         raise ValueError(f'Connection to filmweb.pl failed: {str(e)}')
-    soup = BeautifulSoup(fw_page.content, "html.parser")
-    fw_preview = soup.find('div', class_='userPreview')
-    fw_uid = fw_preview['data-id']
+    try:
+        soup = BeautifulSoup(fw_page.content, "html.parser")
+        fw_preview = soup.find('div', class_='userPreview')
+        fw_uid = fw_preview['data-id']
+    except Exception as e:
+        raise ValueError(f'Error parsing profile page: {str(e)}')
     assert fw_uid != '$user.id', f'user {user_name} does not exist'
     return fw_uid
-
-print(get_user_id('pieca'))
