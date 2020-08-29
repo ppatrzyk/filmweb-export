e6f9076710e6c5ea9eb8771f85576878d2b3fe52
ppatrzyk <piotr@patrzyk.me>
2020-08-29 11:39:39 +0200

    trash old pyfilmweb code

--- filmweb-export/utils.py
+++ filmweb-export/utils.py
@@ -18,31 +18,3 @@ def get_user_id(user_name):
         raise ValueError(f'Error parsing profile page: {str(e)}')
     assert fw_uid != '$user.id', f'user {user_name} does not exist'
     return fw_uid
-
-### copied - redo
-
-fw = Filmweb()
-fw.login(name = fw_name, password = fw_password)
-
-user_obj = User(fw, name = user, uid = fw_id)
-raw_data = user_obj.get_film_votes()
-for entry in raw_data:
-    entry['film'] = entry['film'].uid
-
-movie_obj = Film(fw, uid = entry.pop('film'))
-movie = movie_obj.get_info()
-
-if type(movie.get('url')) is str:
-    movie['url'] = re.sub(
-        r'(?<=web\.pl/film/).+?(?=/$)',
-        lambda match: r'{}'.format(quote_plus(match.group(0))), re.sub('discussion$', '', movie.get('url'))
-    )
-
-dir_list = movie_obj.get_persons(role_type = 'Reżyser')
-dir_list = [director for director in dir_list if director.get('role_extra_info') is None]
-if dir_list:
-    director = dir_list[0]['person'].get_info()
-    if type(director.get('birth_place')) is str:
-        director['birth_place'] = re.sub('\(obecn.*?\)', '', director.get('birth_place'))
-else:
-    director = {'name':None, 'sex':None, 'birth_place':None, 'birth_date':None, 'death_date':None}
