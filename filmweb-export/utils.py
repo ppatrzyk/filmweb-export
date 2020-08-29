d4ca41e711256f8a7f76c0b68f753f2cb68f97f8
ppatrzyk <piotr@patrzyk.me>
2020-08-29 12:03:50 +0200

    login params

--- filmweb-export/utils.py
+++ filmweb-export/utils.py
@@ -26,14 +26,20 @@ def get_user_id(user_name):
     assert fw_uid != '$user.id', f'user {user_name} does not exist'
     return fw_uid
 
-def get_params():
+def get_params(user, password):
     """
     Format request parameters
     """
+    # TODO take method as param for now login only
+    method = f'login [\"{user}\",\"{password}\",1]\n'
+    signature = f'{method}{API_ID}{API_KEY}'
+    md5 = hashlib.md5()
+    md5.update(signature.encode('utf-8'))
+    signature = f'{API_VERSION},{md5.hexdigest()}'
     params = {
         'version': API_VERSION,
         'appId': API_ID,
-        'methods': None,
-        'signature': None
+        'methods': method,
+        'signature': signature,
     }
     return params
