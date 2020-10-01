96f24d92b99f5bbf803076d5b9c935fc87c48934
Piotr Patrzyk <piotr@patrzyk.me>
2020-10-01 20:45:24 +0200

    add multiprocessing calls

--- filmweb/main.py
+++ filmweb/main.py
@@ -10,6 +10,8 @@ Options:
 from docopt import docopt
 from math import ceil
 import requests
+from multiprocessing import Pool
+import tqdm
 from utils import (
     get_movie_ratings,
     get_page,
@@ -30,6 +32,10 @@ def main():
     login(session, user, password)
     votes = get_vote_count(get_page(session, user))
     pages = ceil(votes/MOVIES_PER_PAGE)
+    pool = Pool(processes=4)
+    raw_responses = tuple(tqdm.tqdm(pool.imap_unordered(get_page, range(1, pages+1)), total=pages))
+    movies = tuple(tqdm.tqdm(pool.imap_unordered(get_movie_ratings, raw_responses), total=pages))
+    print(movies)
     logout(session)
     session.cookies.clear()
     session.close()
