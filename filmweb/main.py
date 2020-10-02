cc33a5ed8b49e0102f4e7a457c318a948db1d07b
Piotr Patrzyk <piotr@patrzyk.me>
2020-10-02 16:53:57 +0200

    bar removal fix2

--- filmweb/main.py
+++ filmweb/main.py
@@ -42,7 +42,7 @@ def main():
     pool = Pool(processes=PARALLEL_PROC)
     get_page_args = ((deepcopy(session), get_user, page) for page in range(1, pages+1))
     raw_responses = tuple(tqdm.tqdm(pool.starmap(get_page, get_page_args), total=pages))
-    movies = tuple(pool.map(get_movie_ratings, raw_responses), total=pages)
+    movies = tuple(pool.map(get_movie_ratings, raw_responses))
     pool.close()
     logout(session)
     session.cookies.clear()
