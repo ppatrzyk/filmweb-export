29e4510377b26958bb8c01d3126e8f040a8bf402
Piotr Patrzyk <piotr@patrzyk.me>
2020-10-02 17:49:30 +0200

    simplify

--- setup.py
+++ setup.py
@@ -13,14 +13,9 @@ setup(name='filmweb',
     python_requires='~=3.7',
     install_requires=[
         'beautifulsoup4>=4.9.1',
-        'certifi>=2020.6.20',
-        'chardet>=3.0.4',
         'docopt>=0.6.2',
-        'idna>=2.10',
         'requests>=2.24.0',
-        'soupsieve>=2.0.1',
         'tqdm>=4.50.0',
-        'urllib3>=1.25.10',
     ],
     entry_points={
         'console_scripts': [
