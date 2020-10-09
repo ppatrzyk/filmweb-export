d973842f38f33b65ba22d0330f1f8c92f38cf9eb
Piotr Patrzyk <piotr@patrzyk.me>
2020-10-09 19:39:15 +0200

    fix description

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='filmweb',
     version='0.1',
     license='MIT',
     description='Export movie ratings from filmweb.pl',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords=['filmweb', 'movie', 'crawler'],
     author='Piotr Patrzyk',
     url='https://github.com/ppatrzyk/filmweb-export',
     packages=['filmweb'],
-    python_requires='~=3.7',
+    python_requires='>=3.7',
     install_requires=[
         'beautifulsoup4>=4.9.1',
         'docopt>=0.6.2',
