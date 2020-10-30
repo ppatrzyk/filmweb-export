b6c7e6235e8ac21bd541f128278f613300166b6d
ppatrzyk <piotr@patrzyk.me>
2020-10-30 17:47:17 +0100

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='filmweb',
-    version='0.2',
+    version='0.3',
     license='MIT',
     description='Export movie ratings from filmweb.pl',
     long_description=long_description,
